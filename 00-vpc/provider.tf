531e8a8f37c947782cd57357de215b5e5a1665d5
Vignesh Kattamudi <153530170+vigneshkattamudi@users.noreply.github.com>
2025-12-18 20:38:50 +0000

    Change S3 bucket name for Terraform backend

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "84s-remote-state-dev"
+    bucket = "dsops84-roboshop-dev"
     key    = "roboshop-dev-vpc"
     region = "us-east-1"
     encrypt        = true
