e82ddaf617cbedd4abb84e5231849c54eb1baf22
Vignesh Kattamudi <153530170+vigneshkattamudi@users.noreply.github.com>
2025-12-18 20:37:32 +0000

    Change VPC module source repository

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "git::https://github.com/daws-84s/terraform-aws-vpc.git?ref=main"
+    source = "git::https://github.com/vigneshkattamudi/terraform-aws-vpc.git?ref=main"
     project = var.project
     environment = var.environment
     public_subnet_cidrs = var.public_subnet_cidrs
