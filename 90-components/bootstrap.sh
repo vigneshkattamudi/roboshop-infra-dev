96e6d561bc77020c7b44a6ac09bd219fa9edc4a1
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-12-29 23:02:43 +0000

    terraform

--- 90-components/bootstrap.sh
+++ 90-components/bootstrap.sh
@@ -2,4 +2,4 @@
 
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/vigneshkattamudi/ansible-roboshop-roles-tf.git -e component=$1 -e env=$2 main.yaml
+ansible-pull -U https://github.com/vigneshkattamudi/ansible-roles-tf.git -e component=$1 -e env=$2 main.yaml
