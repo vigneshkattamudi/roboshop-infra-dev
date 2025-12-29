5b52b4478d045405d7bd5909f0ba566f7f491165
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-12-29 22:44:49 +0000

    terraform

--- create-all.sh
+++ create-all.sh
@@ -7,13 +7,13 @@ set -e
 modules=(
   # "00-vpc"             # VPC first
   # "10-sg"              # Depends on VPC
-  "20-bastion"
+  # "20-bastion"
   #"30-vpn"             # Depends on SGs & VPC
-  #"40-databases"       # Depends on SGs & subnets
+  "40-databases"       # Depends on SGs & subnets
   # "50-backend-alb"     # Backend ALB
   # "60-acm"             # ACM certificates (for ALBs)
   # "70-frontend-alb"    # Frontend ALB (depends on ACM)
-  #"60-catalogue"       # Backend service
+  "60-catalogue"       # Backend service
   #"80-user"   # Backend service
   #"90-components"
   
