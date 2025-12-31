e444823625c93a1c3fe48a3b1b3a4316891211a1
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-12-31 11:26:15 +0000

    terraform

--- 70-frontend-alb/main.tf
+++ 70-frontend-alb/main.tf
@@ -35,7 +35,7 @@ resource "aws_lb_listener" "frontend_alb" {
 
 resource "aws_route53_record" "frontend_alb" {
   zone_id = var.zone_id
-  name    = "${var.environment}.${var.zone_name}" #dev.daws84s.site
+  name    = "${var.environment}.${var.zone_name}" #dev.dsops84.space
   type    = "A"
 
   alias {
