c473ddf97c9aad7fc712dbfaf6f03dc4abaeea97
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2026-01-13 18:01:13 +0000

    roboshop

--- create-all.sh
+++ create-all.sh
@@ -5,18 +5,18 @@ set -e
 
 # Terraform directories in correct creation order
 modules=(
-  # "00-vpc"             # VPC first
-  # "10-sg"              # Depends on VPC
+   "00-vpc"             # VPC first
+   "10-sg"              # Depends on VPC
   # "20-bastion"
-  #"30-vpn"             # Depends on SGs & VPC
-  "40-databases"       # Depends on SGs & subnets
+  "30-vpn"             # Depends on SGs & VPC
+  #"40-databases"       # Depends on SGs & subnets
   #"50-backend-alb"     # Backend ALB
-  "60-acm"             # ACM certificates (for ALBs)
-  "70-frontend-alb"    # Frontend ALB (depends on ACM)
+  # "60-acm"             # ACM certificates (for ALBs)
+  # "70-frontend-alb"    # Frontend ALB (depends on ACM)
   #"60-catalogue"       # Backend service
   #"80-user"   # Backend service
-  "90-components"
-  "91-cdn"
+  # "90-components"
+  # "91-cdn"
   
   
 )
